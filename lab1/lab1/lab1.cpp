232a0d4b6ef5da82e6a33901929079d06194f527
Mikhail <96204421+spinmozgJr@users.noreply.github.com>
2022-09-13 22:52:28 +0300

    Add files via upload

---
+++ lab1/lab1/lab1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <time.h>
+
+const int LENGTH_OF_STRING = 1000;
+const int START_OF_ALPHABET = 97;
+const int END_OF_ALPHABET = 122;
+
+std::string generateString();
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+	std::string str = generateString();
+	
+	std::cout << str << std::endl;
+
+	int iterations = 0;
+	int all_itearions = 0;
+	int max_iterations = 0;
+	for (int i = 0; i < LENGTH_OF_STRING; i++) {
+		for (int j = START_OF_ALPHABET; j <= END_OF_ALPHABET; j++) {
+			iterations++;
+			all_itearions++;
+
+			if (str[i] == (char)j) {
+				if (max_iterations < iterations) {
+					max_iterations = iterations;
+					iterations = 0;
+				}
+				break;
+			}
+		}
+		iterations = 0;
+	}
+
+	std::cout << "Всего итераций: " << all_itearions << std::endl;
+	std::cout << "Максимальное количество попыток угадать один символ: " << max_iterations << std::endl;
+}
+
+/*
+ * Создание строки из маленьких букв английского алфавита
+ *
+ * @return возвращает строку длиной LENGTH_OF_STRING, состоящую из маленьких букв английского алфвита
+ */
+std::string generateString() {
+	srand(time(0));
+	std::string result = "";
+	for (int i = 0; i < LENGTH_OF_STRING; i++)
+	{
+		result += (char)(START_OF_ALPHABET + rand() % (END_OF_ALPHABET - START_OF_ALPHABET + 1));
+	}
+	return result;
+}
